f94f95a318709df74aa344b83217497c274ba25d
rakoo <matthieu.rakotojaona@gmail.com>
2012-02-25 20:24:36 +0100

    finalized version

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,55 +1,50 @@
 // Apache 2.0 J Chris Anderson 2011
-$(function() {   
-    // friendly helper http://tinyurl.com/6aow6yn
-    $.fn.serializeObject = function() {
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function() {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
-        });
-        return o;
-    };
+$(document).ready(function() {   
+	// The db
+	var path = unescape(document.location.pathname).split('/'),
+		design = path[3],
+		db = $.couch.db(path[1]);
 
-    var path = unescape(document.location.pathname).split('/'),
-        design = path[3],
-        db = $.couch.db(path[1]);
-    function drawItems() {
-        db.view(design + "/search_index", {
-            descending : "true",
-            limit : 50,
-            update_seq : true,
-            success : function(data) {
-                setupChanges(data.update_seq);
-                var them = $.mustache($("#recent-magnet-links").html(), {
-				items : data.rows.map(function(r) {return {"name" : r.value};})
-                });
-                $("#content").html(them);
-            }
-        });
-    };
-    drawItems();
-    var changesRunning = false;
-    function setupChanges(since) {
-        if (!changesRunning) {
-            var changeHandler = db.changes(since);
-            changesRunning = true;
-            changeHandler.onChange(drawItems);
-        }
-    }
-    $.couchProfile.templates.profileReady = $("#new-message").html();
-	$("#create-message").submit(function(e){
-		e.preventDefault();
-		var form = this, doc = $(form).serializeObject();
-		doc.created_at = new Date();
-		doc.profile = profile;
-		db.saveDoc(doc, {success : function() {form.reset();}});
-		return false;
-	}).find("input").focus();
+	// display nice human readable size
+	function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    var posttxt = 0;
+    if (bytes == 0) return 'n/a';
+    while( bytes >= 1024 ) { 
+        posttxt++;
+        bytes = bytes / 1024;
+    }   
+    return bytes.toFixed(2) + " " + sizes[posttxt];
+	}
+
+	//search
+	function search() {
+		db.view(design + "/search_index", {
+			startkey : $("#query").val().toLowerCase(),
+			endkey : $("#query").val().toUpperCase(),
+			include_docs : true,
+			success : function(data) {
+				console.log("here");
+				var them = $.mustache($("#search-result").html(), {
+					num : data.rows.length,
+					items : data.rows.map(function(r) {
+						return {
+							"size" : bytesToSize(r.doc.size),
+							"name" : r.value,
+							"id" : r.doc.id,
+							"hash" : r.doc.hash
+						};
+					})
+				});
+				$("#content").html(them);
+			}
+		});
+	};
+	$("#search-button").click(search());
+	$("#query").keyup(function(event){
+		if (event.which == 13) {
+			search();
+		}  
+	});
+	
 });
