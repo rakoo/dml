76da7fd32fd7b67d65607405edc2230a95609ff4
rakoo <matthieu.rakotojaona@gmail.com>
2012-02-26 20:20:16 +0100

    split names in search_index view with _

--- views/search_index/map.js
+++ views/search_index/map.js
@@ -2,7 +2,7 @@ function(doc) {
 	if (doc.name){
 		var tokens;
 		if (doc.name) {
-			tokens = doc.name.split(/[^a-zA-Z0-9_]+/i);
+			tokens = doc.name.split(/[^a-zA-Z0-9]+/i);
 			tokens.map(function(token) {
 				if (token && token != ""){
 					emit(token, doc.name);
