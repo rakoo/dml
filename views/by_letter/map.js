b436e669d92456114a373104a097bd115e0e5cfb
rakoo <matthieu.rakotojaona@gmail.com>
2012-03-18 19:53:33 +0100

    removed by_letter view

--- views/by_letter/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-	var letters =["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-		for (var letter in letters){
-			if(doc.name.match(letters[letter])){
-				emit(letters[letter],null);
-			}
-		}
-}
-
