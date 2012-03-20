64f0f76153e32a64d4dae18f73d9ff7d1b7217ad
rakoo <matthieu.rakotojaona@gmail.com>
2012-03-20 23:10:50 +0100

    Better pagination
    Now we don't pre-fetch the whole ids before paginating.

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
 		design = path[3],
 		db = $.couch.db(path[1]);
 
-    db.info({
-      success: function(data){
-        $("#sidebar").html($.mustache($("#sidebar-mustache").html(),{
-          num: data.doc_count
-        }));
-      }
-    });
+  db.info({
+    success: function(data){
+      $("#sidebar").html($.mustache($("#sidebar-mustache").html(),{
+        num: data.doc_count
+      }));
+    }
+  });
 
 
 	// display nice human readable size
@@ -31,69 +31,96 @@ $(document).ready(function() {
 
 	//search
   function search() {
-  if ($("#query").val() == ""){return;}
-    var terms = $("#query").val().split(' '); // simple for the moment
-    fetch_from_db(terms);
+    if ($("#query").val() == ""){return;}
+    var terms = $("#query").val().toLowerCase().split(' '); // simple for the moment
+    first_fetch_from_db(terms);
   }
 
-  // fetch ids from the dm
-	function fetch_from_db(terms) {
-		db.view(design + "/search_index", {
+
+  // fetch the number of ids from the db
+  function first_fetch_from_db(terms) {
+    db.view(design + "/search_index", {
+      keys: terms,
+      stale: "update_after",
+      reduce: true,
+      group: true,
+      success : function(data) {
+
+        // display the number of ids
+        var them = $.mustache($("#result-header-mustache").html(), {
+          num: data.rows.reduce(function(accu,current){
+            return accu + current.value;
+          },0)
+        });
+        $("#result-header").html(them);
+
+        // pagination will start at the beginning of the results
+        var page_index = new Array;
+        display_page(1, page_index, Math.floor(data.rows.length/10),terms);
+      }
+    });
+  };
+
+  // setup and display paginated keys for a page
+  // @page is the number of the page we want to display
+  // @page_index_arg is an array
+  function display_page(page, page_index_arg, num_page, terms){
+
+    // use a clean copy.
+    var page_index = page_index_arg.slice();
+
+    // if fetching from the first page, chances are that page_index is
+    // empty. If there already is something in page_index[1], we can
+    // safely remove it, instead of checking it entirely
+    if (page === 1){page_index[1] = ""};
+
+    db.view(design + "/search_index", {
       keys: terms,
       stale: "update_after",
       reduce: false,
-			success : function(data) {
-        var ids = data.rows.map(function(row){
-          return row.id;
-        });
-        display(ids);
-			}
-		});
-	};
+      startkey_docid: page_index[page],
+      limit: 11,
+      success : function(data) {
 
-  // display ids
-  function display(ids){
-    var them = $.mustache($("#result-header-mustache").html(), {
-      num: ids.length
+        var next_exists = true,
+            prev_exists = true;
+
+        if (data.rows.length < 11){ // last page
+          next_exists = false;
+        }else{
+          page_index.push(data.rows[10].id); // careful ! 11th element is at 10
+        }
+
+        var prev_exists = (function(){return page != 1});
+
+        // display the prev/next
+        $("#result-footer").html($.mustache($("#result-footer-mustache").html(),{
+          next: (function(){return next_exists == true}),
+          page: page,
+          num_page: num_page,
+          prev: (function(){return prev_exists == true})
+        }));
+ 
+        // bind the clicks
+        if (next_exists){
+          $("a[href=#next]").click(function(){
+            display_page(page + 1, page_index, num_page, terms);
+          });
+        }
+        if (prev_exists){
+          $("a[href=#prev]").click(function(){
+            display_page(page - 1, page_index, num_page, terms)});
+        }
+
+
+        // finally, display the goods
+        fetch_and_display(data.rows.slice(0,10).map(function(row){
+          return row.id
+        }), terms);
+
+      }
     });
-    $("#result-header").html(them);
 
-    display_paginated(1,ids) // when pagination is called, everything is set up in place
-
-  }
-
-  // setup and display paginated keys for a page
-  function display_paginated(page, from_ids){
-      var prev_exists = true, next_exists = true;
-      var last_page = Math.ceil(from_ids.length / 10);
-
-      if (page === 1){prev_exists = false}
-      if (page === last_page){next_exists = false}
-
-      var last_id;
-      if (page === last_page){
-        last_id = from_ids.length
-      }else{
-        last_id = page * 10
-      }
-      paginated_keys = from_ids.slice( (page - 1) * 10, last_id);
-
-      $("#result-footer").html($.mustache($("#result-footer-mustache").html(),{
-        next: (function(){return next_exists == true}),
-        page: page,
-        num_page: last_page,
-        prev: (function(){return prev_exists == true})
-      }));
-
-
-      if (next_exists){
-        $("a[href=#next]").click(function(){display_paginated(page + 1, from_ids)});
-      }
-      if (prev_exists){
-        $("a[href=#prev]").click(function(){display_paginated(page - 1, from_ids)});
-      }
-
-      fetch_and_display(paginated_keys);
   }
 
 
@@ -120,12 +147,12 @@ $(document).ready(function() {
   }
 
 
-	$("#search-button").click(search);
-	$("#query").keyup(function(event){
-		if (event.which == 13) {
-			search();
-		}  
-	});
+  $("#search-button").click(search);
+  $("#query").keyup(function(event){
+    if (event.which == 13) {
+      search();
+    }  
+  });
 
-	
+
 });
