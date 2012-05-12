b459f79d1142e7087094f426b5048376e11aabe8
rakoo <matthieu.rakotojaona@gmail.com>
2012-05-13 01:18:15 +0200

    Fix matching with terms specified

--- lists/regexp_check.js
+++ lists/regexp_check.js
@@ -5,10 +5,11 @@ function(head, req) {
 
   while (row = getRow()) {
     count = count + 1;
-    var queries = JSON.parse(req.query.data)["q"];
+    var terms = JSON.parse(req.query.data)["q"];
     var match = true;
-    for (var i = 0; i<queries.length; i++) {
-      if (row.value.indexOf('/' + queries[i] + '/i') === "-1") {
+    for (var i = 0; i<terms.length; i++) {
+      var re = new RegExp(terms[i],'ig');
+      if (!re.test(row.value)) {
         match = false;
         break;
       }
