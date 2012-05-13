88cce72804fec773de42e67e35bc3f1245a8d699
rakoo <matthieu.rakotojaona@gmail.com>
2012-05-13 02:13:23 +0200

    Dedupe ids in the list
    We do this by using a set, which we finally fill in an Array.

--- lists/regexp_check.js
+++ lists/regexp_check.js
@@ -1,23 +1,31 @@
 function(head, req) {
   var row;
-  var count = 0;
-  var ids = [];
+  var set = {};
 
   while (row = getRow()) {
-    count = count + 1;
     var terms = JSON.parse(req.query.data)["q"];
     var match = true;
+
+    // Check if all the terms match the name
     for (var i = 0; i<terms.length; i++) {
       var re = new RegExp(terms[i],'ig');
       if (!re.test(row.value)) {
+        // one of the term doesn't match. We can skip next terms
         match = false;
         break;
       }
     }
+
+    // All the terms have been tested; push if they all match
     if (match == true) {
-      ids.push(row.id);
+      set[row.id] = true;
     }
 
+  }
+
+  var ids = [];
+  for (id in set) {
+    ids.push(id);
   }
   send(JSON.stringify(ids));
 }
